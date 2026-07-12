d432dcd08be050283f71dd94a40226c79c5cd503
yuvi2924 <yuvisharma17654@gmail.com>
2026-07-13 01:25:59 +0530

    Update API base URL to production endpoint

--- habitpro-frontend/src/api/axiosInstance.js
+++ habitpro-frontend/src/api/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: '/api' })
+const api = axios.create({ baseURL: 'https://habit-pro-1.onrender.com/api' })
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token')
